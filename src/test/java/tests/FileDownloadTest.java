2c5aefd0a106e9c5e0ab74771f6219868f14f38d
Lada Petkevich <ladap1999@gmail.com>
2023-01-10 00:12:45 +0300

    update after code review

--- src/test/java/tests/FileDownloadTest.java
+++ src/test/java/tests/FileDownloadTest.java
@@ -7,16 +7,19 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 public class FileDownloadTest extends BaseTest {
 
     @Test
     public void fileDownloadTest() throws InterruptedException {
         driver.get("http://the-internet.herokuapp.com/download");
 
-        WebElement link = driver.findElement(By.linkText("sample.png"));
+        WebElement link = driver.findElement(By.xpath("//a[@href = 'download/sample.png']"));
         link.click();
 
         String nameFile = link.getText();
-        Assert.assertTrue(waitsService.waitForExistsFile(System.getProperty("user.dir")+"\\" + nameFile));
+        Assert.assertTrue(waitsService.waitForExistsFile(System.getProperty("user.dir")
+                + File.separator + nameFile));
     }
 }
--- src/test/java/tests/FileUploadTest.java
+++ src/test/java/tests/FileUploadTest.java
@@ -2,22 +2,34 @@ package tests;
 
 import baseEntities.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.nio.file.Path;
 
 public class FileUploadTest extends BaseTest {
 
     @Test
     public void fileUploadTest() {
+        String pathToFile = "";
         driver.get("http://the-internet.herokuapp.com/upload");
 
-        WebElement uploadWebElement = waitsService.waitForExists(By.xpath("//input[@type='file']"));
-        String pathToFile = FileUploadTest.class.getClassLoader().getResource("cable.png").getPath().substring(1);
-        uploadWebElement.sendKeys(pathToFile);
+        WebElement uploadWebElement = waitsService
+                .waitForExists(By.xpath("//input[@type='file']"));
+
+        try {
+            pathToFile = FileUploadTest.class.getClassLoader().getResource("cable.png").getPath();
+            uploadWebElement.sendKeys(pathToFile);
+        } catch (InvalidArgumentException exception) {
+            pathToFile = FileUploadTest.class.getClassLoader().getResource("cable.png").getPath().substring(1);
+            uploadWebElement.sendKeys(pathToFile);
+        }
+
         driver.findElement(By.id("file-submit")).submit();
 
         Assert.assertEquals(waitsService.waitsForVisibilityBy(By.id("uploaded-files"))
-                        .getText().trim(),"cable.png");
+                .getText().trim(), "cable.png");
     }
 }
