f39fd9f160b2fe50634af8170db41ff725b53e6f
Lada Petkevich <ladap1999@gmail.com>
2023-02-09 17:07:42 +0300

    create restApi

--- src/test/java/models/Project.java
+++ src/test/java/models/Project.java
@@ -1,84 +1,65 @@
 package models;
 
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 public class Project {
+
+    @SerializedName(value = "name")
     private String name;
     private String announcement;
+
+    @SerializedName(value = "suite_mode")
     private int type;
+
+    @SerializedName(value = "show_announcement")
     private boolean showAnnouncement;
     private boolean deleted;
 
+//    public Project(String name, String announcement, int type, boolean showAnnouncement, boolean deleted) {
+//        this.name = name;
+//        this.announcement = announcement;
+//        this.type = type;
+//        this.showAnnouncement = showAnnouncement;
+//        this.deleted = deleted;
+//    }
+
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getAnnouncement() {
         return announcement;
     }
 
-    public void setAnnouncement(String announcement) {
-        this.announcement = announcement;
-    }
-
     public int getType() {
         return type;
-    }
-
-    public void setType(int type) {
-        this.type = type;
     }
 
     public boolean isShowAnnouncement() {
         return showAnnouncement;
     }
 
-    public void setShowAnnouncement(boolean showAnnouncement) {
-        this.showAnnouncement = showAnnouncement;
-    }
-
     public boolean isDeleted() {
         return deleted;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAnnouncement(String announcement) {
+        this.announcement = announcement;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setShowAnnouncement(boolean showAnnouncement) {
+        this.showAnnouncement = showAnnouncement;
+    }
+
     public void setDeleted(boolean deleted) {
         this.deleted = deleted;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Project project = (Project) o;
-
-        if (type != project.type) return false;
-        if (showAnnouncement != project.showAnnouncement) return false;
-        if (!Objects.equals(name, project.name)) return false;
-        return Objects.equals(announcement, project.announcement);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (announcement != null ? announcement.hashCode() : 0);
-        result = 31 * result + type;
-        result = 31 * result + (showAnnouncement ? 1 : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Project{" +
-                "name='" + name + '\'' +
-                ", announcement='" + announcement + '\'' +
-                ", type=" + type +
-                ", showAnnouncement=" + showAnnouncement +
-                ", deleted=" + deleted +
-                '}';
     }
 }
